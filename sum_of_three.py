98587ab190a8aae5bbf3b1002d351186d10815b2
Dilay Bezazoğlu <dilayb5@gmail.com>
2025-12-16 15:24:58 +0300

    Solve sum_of_three challange Ilk proje tamamladı

--- sum_of_three.py
+++ sum_of_three.py
@@ -2,6 +2,6 @@
 
 def sum3(first_number, second_number, third_number):
     """
-    Takes three numbers and returns the sum of those numbers
+    Returns the sum of three numbers.
     """
-    pass  # YOUR CODE HERE
+    return first_number + second_number + third_number
